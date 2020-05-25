cf04ab282918cd8df628b9282e7575b07ba976c9
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-05-25 19:06:31 +0100

    added greetings to main

---
+++ bin/main.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/bot.rb'
+require_relative '../lib/love.rb'
+require_relative '../lib/inspire.rb'
+
+puts 'WELCOME TO OUR TELEGRAM BOT'
+puts 'We love to give you some inspirational and love quotes to spices up your day'
+puts '...loading the bot is now online... use ctrl-c to stop the bot'
+Bot.new
