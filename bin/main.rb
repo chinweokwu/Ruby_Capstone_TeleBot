fd55fb3af065df5f2be49964d6effa31e6336ad5
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-05-31 16:05:01 +0100

    fixed stickler errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/bot.rb'
+require 'dotenv'
 
 puts 'Hello! , Welcome to telegram bot'
 puts '----------------------------------------------'
 puts 'This is a bot to help you stay inspired by giving you inspirational quotes'
 puts 'The bot can also keep you entertained by providing you with love quotes'
-sleep(0.6)
+sleep(0.8)
 puts '----------------------------------------------'
 puts '...loading the bot is now online... use ctrl-c to stop the bot'
+
 BotRun.new
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,49 +1,61 @@
 require 'telegram/bot'
 require_relative 'love.rb'
 require_relative 'inspire.rb'
+require 'uri'
+require 'net/http'
+require 'openssl'
 
 # rubocop:disable Layout/LineLength
 # rubocop:disable Metrics/BlockLength
+# rubocop:disable  Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength
+
 class BotRun
   def initialize
-token = '1239771112:AAFc73gTTkpsr9aoO4rNMPo-qKeKfsmQpyI'
+    token = '1239771112:AAFc73gTTkpsr9aoO4rNMPo-qKeKfsmQpyI'
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/hi'
-      bot.api.send_message(
-        chat_id: message.chat_id,
-        text: "Hello!!! #{message.from.first_name}. Welcome to a love and inspiration chatbot created by Morah Paul."
-      )
-    when '/start'
-      bot.api.send_message(
-        chat_id: message.chat_id,
-        text: 'Use /start to start the bot, /stop to end the bot, /inspire to get a diffrent inspirational quote, /love to get a love quote'
-      )
-    when '/stop'
-      bot.api.send_message(
-        chat_id: message.chat_id,
-        text: "BYE #{message.from.first_name}"
-      )
-    when '/love'
-      love_quote = LoveQuotes.new
+    Telegram::Bot::Client.run(token) do |bot|
+      begin
+       bot.listen do |message|
+         case message.text
+         when '/hi'
+           bot.api.send_message(
+             chat_id: message.chat_id,
+             text: "Hello!!! #{message.from.first_name}. Welcome to a love and inspiration chatbot created by Morah Paul."
+           )
+         when '/start'
+           bot.api.send_message(
+             chat_id: message.chat_id,
+             text: 'Use /start to start the bot, /stop to end the bot, /inspire to get a diffrent inspirational quote, /love to get a love quote'
+           )
+         when '/stop'
+           bot.api.send_message(
+             chat_id: message.chat_id,
+             text: "BYE #{message.from.first_name}"
+           )
+         when '/love'
+           love_quote = LoveQuotes.new
 
-      bot.api.send_message(
-        chat_id: message.chat_id,
-        text: love_quote.luv
-      )
-    when '/inspire'
-      inspire = InspireQuotes.new
+           bot.api.send_message(
+             chat_id: message.chat_id,
+             text: love_quote.luv
+           )
+         when '/inspire'
+           inspire = InspireQuotes.new
 
-      bot.api.send_message(
-        chat_id: message.chat_id,
-        text: inspire.inspire_quotes
-      )
+           bot.api.send_message(
+             chat_id: message.chat_id,
+             text: inspire.inspire_quotes
+           )
+         end
+       end
+      rescue StandardError
+        retry
+     end
     end
   end
 end
-end
-end
+# rubocop:enable  Metrics/CyclomaticComplexity
 # rubocop:enable Layout/LineLength
 # rubocop:enable Metrics/BlockLength
+# rubocop:enable Metrics/MethodLength
