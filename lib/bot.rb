0689147cf26bc0b6303d46db5867a0e8b096e3b1
chinwe <44407831+chinweokwu@users.noreply.github.com>
2020-06-01 20:08:07 +0100

    made changes on bot.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -22,31 +22,31 @@ class BotRun
          case message.text
          when '/hi'
            bot.api.send_message(
-             chat_id: message.chat_id,
+             chat_id: message.chat.id,
              text: "Hello!!! #{message.from.first_name}. Welcome to a love and inspiration chatbot created by Morah Paul."
            )
          when '/start'
            bot.api.send_message(
-             chat_id: message.chat_id,
+             chat_id: message.chat.id,
              text: 'Use /start to start the bot, /stop to end the bot, /inspire to get a diffrent inspirational quote, /love to get a love quote'
            )
          when '/stop'
            bot.api.send_message(
-             chat_id: message.chat_id,
+             chat_id: message.chat.id,
              text: "BYE #{message.from.first_name}"
            )
          when '/love'
            love_quote = LoveQuotes.new
 
            bot.api.send_message(
-             chat_id: message.chat_id,
+             chat_id: message.chat.id,
              text: love_quote.luv
            )
          when '/inspire'
            inspire = InspireQuotes.new
 
            bot.api.send_message(
-             chat_id: message.chat_id,
+             chat_id: message.chat.id,
              text: inspire.inspire_quotes
            )
          end
